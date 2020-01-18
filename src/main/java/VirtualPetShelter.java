0d79d8c7a3728c29bba9d686d96c4b15acf58c51
mcheich <mcheich@gmail.com>
2020-01-18 06:29:18 -0500

    Working on Test to drive constructor of vpshelter and vpet classes

---
+++ src/main/java/VirtualPetShelter.java
@@ -0,0 +1,9 @@
+
+public class VirtualPetShelter {
+
+	public void intake(String name, String description) {
+
+		
+	}
+
+}
---
+++ src/test/java/VirtualPetSheltertest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VirtualPetSheltertest {
+
+	@Test
+	public void intakeAdds1PetToTheVirtualPetShelter() {
+	
+	}
+
+}
